be9982c9524e8e6b1b617e184e6c33ee325b4001
LMF709268224 <709268224@qq.com>
2023-06-29 17:18:02 +0800

    add unique to table

--- storage/mysql/base.go
+++ storage/mysql/base.go
@@ -1,9 +1,8 @@
-package storage
+package mysql
 
 import (
 	"database/sql"
 	"fmt"
-	"strings"
 	"time"
 
 	"vps-provider/config"
@@ -68,47 +67,4 @@ func initTables() error {
 	tx.MustExec(fmt.Sprintf(cUsersTable, tableNameUser))
 
 	return tx.Commit()
-}
-
-func GetQueryDataList(sqlClause string, args ...interface{}) ([]map[string]string, error) {
-	rows, err := DB.Query(sqlClause, args...)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	columns, err := rows.Columns()
-	if err != nil {
-		return nil, err
-	}
-
-	values := make([]sql.RawBytes, len(columns))
-	scanArgs := make([]interface{}, len(columns))
-	for i := range values {
-		scanArgs[i] = &values[i]
-	}
-
-	dataList := make([]map[string]string, 0)
-	for rows.Next() {
-		err = rows.Scan(scanArgs...)
-		if err != nil {
-			return nil, err
-		}
-
-		data := make(map[string]string)
-		for i, col := range values {
-			//			if col == nil {
-			//				continue
-			//			}
-
-			key := columns[i]
-			key = strings.ToLower(key)
-			data[key] = string(col)
-
-		}
-		//		log.Info(&data)
-		dataList = append(dataList, data)
-	}
-
-	return dataList, nil
 }
--- storage/mysql/tables.go
+++ storage/mysql/tables.go
@@ -2,8 +2,8 @@ package mysql
 
 var cUsersTable = `
     CREATE TABLE if not exists %s (
-		uuid          VARCHAR(128) NOT NULL,
-		user_name     VARCHAR(128) NOT NULL,
+		uuid          VARCHAR(128) NOT NULL UNIQUE,
+		user_name     VARCHAR(128) NOT NULL UNIQUE,
 		pass_hash     VARCHAR(128) NOT NULL,
 		created_at    DATETIME     DEFAULT CURRENT_TIMESTAMP,
 		PRIMARY KEY (uuid)
